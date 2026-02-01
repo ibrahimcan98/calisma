f176e58bc3d6334b686954d7c22c1fb22251e2c4
İbrahim <ibrahimcanonder9895@gmail.com>
2026-02-01 08:19:00 +0000

    ismi ekleye basiyorum ama bisi eklemiyor

--- src/components/add-transaction-sheet.tsx
+++ src/components/add-transaction-sheet.tsx
@@ -124,7 +124,7 @@ export function AddTransactionSheet({
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Tür</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="İşlem türünü seçin" />
@@ -171,7 +171,7 @@ export function AddTransactionSheet({
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Kategori</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="Bir kategori seçin" />
@@ -217,7 +217,7 @@ export function AddTransactionSheet({
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Alt Kategori</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                      <Select onValueChange={field.onChange} value={field.value}>
                         <FormControl>
                           <SelectTrigger>
                             <SelectValue placeholder="Bir alt kategori seçin" />
